c7ac7c7f18cc903759362886d47058da7b772b21
H-Simpkins <harryshafersimpkins@gmail.com>
2023-09-25 16:19:19 -0500

    Started Hero

--- besch-bike-fit/src/App.jsx
+++ besch-bike-fit/src/App.jsx
@@ -12,14 +12,16 @@ function App() {
       <button className="nav-btn">Request Appointment</button>
       <button className="nav-btn">Contact Us</button>
     </header>
-    <div className="motto">
-        <h1 className= "lineOne">The trails are calling,</h1> 
-        <h1 className="lineTwo">Be ready for them.</h1>
-    </div>
-    <div>
-      <button className="schedule-btn">Schedule Now!</button>
-    </div>
-
+    <body className="body">
+      <div className="hero">
+        <div className="motto">
+          <h1 className= "lineOne">The trails are calling,</h1> 
+          <h1 className="lineTwo">Be ready for them.</h1>
+          <button className="schedule-btn">Schedule Now!</button>
+        </div>
+        <div className="hero-img"></div>
+      </div>
+    </body>
     </>
   )
 }
