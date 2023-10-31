ae22283cc359780db3097efebd8a69b7b7d9117d
H-Simpkins <harryshafersimpkins@gmail.com>
2023-10-31 16:41:21 -0500

    Add haikei header image

--- besch-bike-fit/src/App.jsx
+++ besch-bike-fit/src/App.jsx
@@ -1,4 +1,5 @@
 import logo from "./assets/logo.png";
+import peaks from "./assets/layered-peaks-haikei.svg";
 import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
 import { About } from "./About";
@@ -12,28 +13,34 @@ function App() {
     <>
       <header>
         <img src={logo} className="logo" alt="logo" />
-        <nav className="nav-wrapper">
-          <div className="main-nav">
-            <Link to="/" className="nav-btn">
-              Home
-            </Link>
-            <Link to="/About" className="nav-btn">
-              About
-            </Link>
-            <Link to="/Services" className="nav-btn">
-              Services
-            </Link>
-            <Link to="/Contact" className="nav-btn">
-              Contact Us
-            </Link>
-          </div>
-          <div className="nav-socials">
-            <span className="nav-social-words">Follow me:</span>
-            <a className="instagram" href="https://www.instagram.com/beschbikefit/">
-            <Instagram />
-            </a>
-          </div>
-        </nav>
+        <div className="header-container">
+          <img src={peaks} className="header-img" alt="peaks" />
+          <nav className="nav-wrapper">
+            <div className="main-nav">
+              <Link to="/" className="nav-btn">
+                Home
+              </Link>
+              <Link to="/About" className="nav-btn">
+                About
+              </Link>
+              <Link to="/Services" className="nav-btn">
+                Services
+              </Link>
+              <Link to="/Contact" className="nav-btn">
+                Contact Us
+              </Link>
+            </div>
+            <div className="nav-socials">
+              <span className="nav-social-words">Follow me:</span>
+              <a
+                className="instagram"
+                href="https://www.instagram.com/beschbikefit/"
+              >
+                <Instagram />
+              </a>
+            </div>
+          </nav>
+        </div>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
